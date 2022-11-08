2b0f231bbfb39fcdddea76170e91f36fdb78810b
Antonio Sein Jagou <antonioseinjagou95@gmail.com>
2022-11-08 14:10:52 -0600

    HTTP Server config

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+
+dotenv.config({path: 'config.env'})
+
+const PORT = process.env.PORT || 8080
+
+app.get('/',(req,res)=>{
+    res.send("CRUD App");
+})
+
+app.listen(PORT, ()=> {console.log(`Server is running on http://localhost:${PORT}`)});
